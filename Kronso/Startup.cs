928a850d37aa3ad5371246b4e3bfe2f11ecae4ab
pmgesualdi <pmgesualdi@gmail.com>
2017-08-19 12:28:39 -0300

    Implementation of VS ASP.NET MVC Template

---
+++ Kronso/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kronso.Startup))]
+namespace Kronso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
